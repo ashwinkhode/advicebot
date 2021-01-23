b97f2f01551d505225db2783373d0a207dc6cc2c
Ashwin Khode <ashwin.khode1@gmail.com>
2021-01-23 18:39:02 +0530

    discord bot

--- bot.js
+++ bot.js
@@ -2,6 +2,9 @@ console.log('Starting bot');
 
 const fetch = require('node-fetch');
 const Twit = require('twit');
+const discord = require('discord.js')
+const client = new discord.Client()
+
 const config = {
     consumer_key: process.env.consumer_key,
     consumer_secret: process.env.consumer_secret,
@@ -9,23 +12,23 @@ const config = {
     access_token_secret: process.env.access_token_secret
 }
 
-var T = new Twit(config);
-//const timeout = Math.floor((Math.random())*50000000)
-const timeout = 1000
-console.log(timeout);
-let counter = 1;
-
-function getAdvice() {
-    const url = 'https://api.adviceslip.com/advice';
-    fetch(url)
-        .then((response) => response.json()
-            .catch((err) => console.log(err)))
-        .then(function (response) {
-            //T.post('statuses/update', { status: `Free Advice : ${response.slip.advice} #free #advice` }, function(err, data, res) {
-            T.post('statuses/update', {status: `#100FREEiPhone12 ${counter + 1} Merry Christmas & happy new year #100FREEiPhone12 #unboxTherapy #christmas #free #apple #iphone12`}, function (err, data, res) {
-                console.log(err);
-            })
-        })
+const discordConfig = {
+    id: process.env.did,
+    clikey: process.env.clikey,
+    key: process.env.key,
+    token: process.env.token
 }
 
-setInterval(getAdvice, timeout)
+var T = new Twit(config);
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong')
+    }
+})
+
+client.login(discordConfig.token)
+
+function getAdvice() {
+    console.log('running')
+}
